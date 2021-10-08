407245afdaad58b1d5cf300d0db9a91ec50aee85
Péter Ugróczky <peter.ugroczky@stud.uni-corvinus.hu>
2021-10-08 10:29:16 +0200

    Excel namespace hozzáadása

--- UserMaintenance/ExcelExport/Form1.cs
+++ UserMaintenance/ExcelExport/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection; //Excelhez ez a két névtér kell
 
 namespace ExcelExport
 {
     public partial class Form1 : Form
     {
+        
+
         RealEstateEntities1 context = new RealEstateEntities1();
         List<Flat> lakasok;
         public Form1()
