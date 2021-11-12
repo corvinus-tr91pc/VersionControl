cda99e78b61a831fd9c555357478f25bb795834a
Péter Ugróczky <peter.ugroczky@stud.uni-corvinus.hu>
2021-11-12 14:04:48 +0100

    Absztrakció felhasználkása a gyárakban

--- GameFactory/GameFactory/Form1.cs
+++ GameFactory/GameFactory/Form1.cs
@@ -34,7 +34,7 @@ namespace GameFactory
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            _balls.Add((Ball)ball);
             ball.Left = -ball.Width;
             mainPanel.Controls.Add(ball);
         }
