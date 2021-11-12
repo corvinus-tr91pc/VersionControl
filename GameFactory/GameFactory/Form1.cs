9f1f6cf998334608b9fe89b34cec3325b36ca664
Péter Ugróczky <peter.ugroczky@stud.uni-corvinus.hu>
2021-11-12 14:11:54 +0100

    Absztrakció felhasználása a futószalagban

--- GameFactory/GameFactory/Form1.cs
+++ GameFactory/GameFactory/Form1.cs
@@ -1,4 +1,5 @@
-﻿using GameFactory.Entities;
+﻿using GameFactory.Abstractions;
+using GameFactory.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,10 +14,10 @@ namespace GameFactory
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
+        private List<Toy> _toys = new List<Toy>();
+        private CarFactory _factory;
 
-        public BallFactory Factory
+        public CarFactory Factory
         {
             get { return _factory; }
             set { _factory = value; }
@@ -28,32 +29,32 @@ namespace GameFactory
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            Factory = new CarFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
-            var ball = Factory.CreateNew();
-            _balls.Add((Ball)ball);
-            ball.Left = -ball.Width;
-            mainPanel.Controls.Add(ball);
+            var toy = Factory.CreateNew();
+            _toys.Add(toy);
+            toy.Left = -toy.Width;
+            mainPanel.Controls.Add(toy);
         }
 
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
