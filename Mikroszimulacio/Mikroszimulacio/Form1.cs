3ee66334e1cb4391e4972f5b4717c0a7d863550b
Péter Ugróczky <peter.ugroczky@stud.uni-corvinus.hu>
2021-12-03 09:35:57 +0100

    Szimuáláció megírása

--- Mikroszimulacio/Mikroszimulacio/Form1.cs
+++ Mikroszimulacio/Mikroszimulacio/Form1.cs
@@ -32,7 +32,7 @@ namespace Mikroszimulacio
             {
                 for (int i = 0; i < Population.Count; i++)
                 {
-                    // szimulacios lepes
+                    SimStep(year, Population[i]);
                 }
 
                 int nbrOfMales = (from x in Population
@@ -43,6 +43,38 @@ namespace Mikroszimulacio
                                     select x).Count();
                 Console.WriteLine(
                     string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+
+            if (!person.IsAlive) return;
+
+            byte age = (byte)(year - person.BirthYear);
+
+
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.Probability).FirstOrDefault();
+
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.Probability).FirstOrDefault();
+
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
             }
         }
 
