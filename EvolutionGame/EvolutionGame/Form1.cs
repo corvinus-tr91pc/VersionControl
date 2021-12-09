dbf93cc86206237f5e2f86bb86899e208e8fe34f
Péter Ugróczky <peter.ugroczky@stud.uni-corvinus.hu>
2021-12-09 19:01:31 +0100

    Játék kipróbálása

--- EvolutionGame/EvolutionGame/Form1.cs
+++ EvolutionGame/EvolutionGame/Form1.cs
@@ -21,6 +21,9 @@ namespace EvolutionGame
 
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            gc.AddPlayer();
+            gc.Start(true);
         }
     }
 }
